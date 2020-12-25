a23db937c239435060bfba114cd026a85351ceba
100pka <ed2k@bk.ru>
2020-12-25 14:06:22 +0300

    Homework 6 MVVM Started implementing ViewModel in MoviesDetails screen

---
+++ app/src/main/java/com/stopkaaaa/androidacademyproject/ui/moviesdetails/MoviesDetailsViewModel.kt
@@ -0,0 +1,8 @@
+package com.stopkaaaa.androidacademyproject.ui.moviesdetails
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+
+class MoviesDetailsViewModel(application: Application) : AndroidViewModel(application) {
+
+}
